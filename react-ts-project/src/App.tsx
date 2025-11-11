a5bc4ef623448608e61c51646540e26f7abac41f
KingOfNorth <mike.fury315@gmail.com>
2025-11-12 00:04:27 +0700

    Хук UseState. Условный рендеринг

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -1,65 +1,61 @@
+import {useState} from "react";
+
+const tracks = [
+    {id: 1, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
+    {id: 2, title: "Instrumental", url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"},
+]
+
+
 export function App() {
-    const tracks = [
-        {id: 1, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {
-            id: 2,
-            title: "Instrumental",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
-        },
-        {id: 3, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {
-            id: 4,
-            title: "Instrumental",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
-        },
-        {id: 5, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {
-            id: 6,
-            title: "Instrumental",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
-        },
-        {id: 7, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {
-            id: 8,
-            title: "Instrumental",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
-        },
-        {id: 9, title: 'Soundtrack', url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {
-            id: 10,
-            title: "Instrumental",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"
-        },
-    ]
-
-    const tasks = [
-        {id: 1, title: "Купить продукты на неделю", isDone: false},
-        {id: 2, title: "Полить цветы", isDone: true},
-        {id: 3, title: "Сходить на тренировку", isDone: false},
-    ]
-
+    const stateManager = useState(null)
+    let selectedTrackId= stateManager[0];
+    let setSelectedTrackId = stateManager[1];
+    const [flag, setFlag] = useState(false)
+    const openPopup = () => {
+        setFlag(true)
+    }
+    const closePopup = () => {
+        setFlag(false)
+    }
+    if (tracks === null || tracks === undefined) {
+        return (
+            <>
+                <h1>MikeSherry Player</h1>
+                <span>Loading...</span>
+            </>
+        )
+    }
+    if (tracks.length === 0) {
+        return (
+            <>
+                <h1>MikeSherry Player</h1>
+                <span>Not found</span>
+            </>
+        )
+    }
     return (
         <div>
             <h1>MikeSherry Player</h1>
             <ul>
+
                 {tracks.map(function (track) {
                     return (
-                        <li key={track.id}>
-                            <div>
+                        <li key={track.id}
+                            style={{border: track.id === selectedTrackId ? '1px solid white' : 'none'}}>
+                            <div onClick={()=>{
+                                setSelectedTrackId(track.id)
+                            }}>
                                 {track.title}
                             </div>
                             <audio src={track.url}
                                    controls></audio>
-                        </li>
-                    )
-                })}
-            </ul>
-            <ul>
-                {tasks.map((task) => {
-                    return (
-                        <li key={task.id}>
-                            <h2>{task.title}</h2>
-                            <input type="checkbox" checked={task.isDone}/>
+                            <button onClick={openPopup}>Показать</button>
+                            {flag && (
+                                <div>
+                                    Попап
+                                    <button onClick={closePopup}>Закрыть попап</button>
+                                </div>
+                            )}
                         </li>
                     )
                 })}
